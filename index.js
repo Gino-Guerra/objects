036e9e3b288fc01f97b93466ef9a92bdc65a9ae0
Gino Guerra <ginoiguerra@gmail.com>
2023-10-05 17:03:06 -0400

    objects and arrays

---
+++ index.js
@@ -0,0 +1,18 @@
+const person = {
+   firstName: "Todd",
+    age: 28,
+    
+}
+console.log(person.firstName)
+person.age++
+console.log(person.age)
+person.lastName = "Albert"
+
+const c12 = [
+{firstName: 'Michael', lastName: "Fisher", points:35 ,fullName: "Michael Fisher"}, 
+{firstName: 'Debbie', lastName: "Divila", points:26 ,fullName: "Debbie Devila"}, 
+{firstName: 'Joelle', lastName: "Johnson", points:24 ,fullName: "Joelle Johnson"}, 
+{firstName: 'Brad', lastName: "Kirks", points:29 ,fullName: "Brad kirks"}, 
+ 
+]
+console.log(c12[3].points)
